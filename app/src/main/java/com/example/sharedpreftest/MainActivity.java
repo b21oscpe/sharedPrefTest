1c31478402ad9f319b449e925127d8908375d5a8
b21oscpe <b21oscpe@student.his.se>
2022-05-03 14:10:16 +0200

    created a sharedpreferences for textview

--- app/src/main/java/com/example/sharedpreftest/MainActivity.java
+++ app/src/main/java/com/example/sharedpreftest/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.sharedpreftest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text_name = findViewById(R.id.text_name);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences preferences = getSharedPreferences("janne", MODE_PRIVATE);
+        String name = preferences.getString("name", "can't get name XD");
+        text_name.setText(name);
     }
 }
